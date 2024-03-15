d7893cff3b8ca806187d63413fa93b365af4b297
Claudia Montero Ramírez <131159036+clmonter@users.noreply.github.com>
2024-03-15 10:46:14 +0100

    Update grid_search.py

--- grid_search.py
+++ grid_search.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 23 09:44:10 2024
+
+@author: clmonter
+"""
+
 from sklearn.model_selection import StratifiedKFold
 from dataset_class import *
 import pandas as pd
